4ad57327d3fbdc0124a1cc0a84dd2592af432c92
doug <linderd@iinet.net.au>
2015-10-15 16:25:31 +0800

    Add explicit calling convention

--- test.c
+++ test.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-void rs_register(void(* value)(int));
-int rs_trigger(int value);
-const char* rs_str(void);
+__cdecl void rs_register(void(* value)(int));
+__cdecl int rs_trigger(int value);
+__cdecl const char* rs_str(void);
 
 void callback(int value) {
   printf("C-callback invoked with value: %d\n", value);
