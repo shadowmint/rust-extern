abcaf089f58bcd2e4c3bd0d68f7096b7a0cfeaf3
doug <linderd@iinet.net.au>
2015-10-18 19:38:03 +0800

    match calling convention explicitly

--- test.c
+++ test.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
-void rs_register(void(* value)(int));
-int rs_trigger(int value);
-const char* rs_str(void);
+#ifdef _WIN64
+  #define __EXT __stdcall
+#elif _WIN32
+  #define __EXT __stdcall
+#else
+  #define __EXT extern
+#endif
+
+__EXT void rs_register(void(* value)(int));
+__EXT int rs_trigger(int value);
+__EXT const char* rs_str(void);
 
 void callback(int value) {
   printf("C-callback invoked with value: %d\n", value);
